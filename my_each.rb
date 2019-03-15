0b28a68c4eade0b9d49e994d5847d60b70e64f60
Brian Tobal <brian.tobal@gmail.com>
2019-03-15 11:05:11 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -1,4 +1,9 @@
 def my_each (arr)
-  # code here
+  #use while to iterate over each member of the Array
+  while arr.lenght < i 
+    
+    i++ 
   
+  
+  #yielding each element contained to a block 
 end
