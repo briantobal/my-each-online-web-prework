390ef696e155a1780e44a8cb0d5d5032b732426c
Brian Tobal <brian.tobal@gmail.com>
2019-03-15 11:06:31 +0000

    Automatically backed up by Learn

--- my_each.rb
+++ my_each.rb
@@ -2,9 +2,10 @@ def my_each (arr)
   #use while to iterate over each member of the Array
   i = 0 
   while arr.lenght < i 
-   yield  
+   yield (collection[i]) 
     i = i+1
-  
+  end 
+  collection 
   
   #yielding each element contained to a block 
 end
